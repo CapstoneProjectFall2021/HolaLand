0f994e8561d1724f9d82eefccaf9250dc708bf1a
Capstone Project <CapstoneProjectFall2021@gmail.com>
2021-09-11 22:08:01 +0700

    add comment

--- holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
@@ -16,6 +16,7 @@ import java.util.List;
 @Controller
 public class MemberController {
 
+    // call MemberService from module traffic
     private final MemberService memberService;
 
     @Autowired
