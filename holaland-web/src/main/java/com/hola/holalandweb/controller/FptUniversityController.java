1d5afbd38d5e5322696fd574c9463a131651cf19
Capstone Project <anhdaica0609@gmail.com>
2021-09-19 03:50:23 +0700

    add module fpt

--- holaland-web/src/main/java/com/hola/holalandweb/controller/FptUniversityController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/FptUniversityController.java
@@ -12,4 +12,22 @@ public class FptUniversityController {
         model.addAttribute("page", 1);
         return "fpt-university";
     }
+
+    @GetMapping("/fpt-university-club")
+    public String goToFptUniversityClub(Model model) {
+        model.addAttribute("page", 2);
+        return "fpt-university";
+    }
+
+    @GetMapping("/fpt-university-department")
+    public String goToFptUniversityDepartment(Model model) {
+        model.addAttribute("page", 3);
+        return "fpt-university";
+    }
+
+    @GetMapping("/fpt-university-lecturers")
+    public String goToFptUniversityLecturers(Model model) {
+        model.addAttribute("page", 4);
+        return "fpt-university";
+    }
 }
