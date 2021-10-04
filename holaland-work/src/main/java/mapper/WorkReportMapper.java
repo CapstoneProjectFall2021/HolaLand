edad642838bf5bf327fea8d1a5ca6bff844805ab
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:47:41 +0700

    code WorkRequestApplyMapper

--- holaland-work/src/main/java/mapper/WorkReportMapper.java
+++ holaland-work/src/main/java/mapper/WorkReportMapper.java
@@ -1,2 +1,20 @@
-package mapper;public class WorkReportMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.WorkJobType;
+import com.hola.holalandwork.entity.WorkReport;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkReportMapper implements RowMapper<WorkReport> {
+    @Override
+    public WorkReport mapRow(ResultSet resultSet, int i) throws SQLException {
+        return WorkReport.builder()
+                .workReportId(resultSet.getInt("work_report_id"))
+                .workReportReason(resultSet.getString("work_report_reason"))
+                .workReportDescription(resultSet.getString("work_report_description"))
+                .workReportStatus(resultSet.getString("work_report_status"))
+                .build();
+    }
 }
