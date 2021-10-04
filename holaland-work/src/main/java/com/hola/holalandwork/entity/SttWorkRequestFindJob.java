0887782da11cd7b67d4fb4354687e20a00f0586d
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 16:00:36 +0700

    add SttWorkRequestFindJob entity

---
+++ holaland-work/src/main/java/com/hola/holalandwork/entity/SttWorkRequestFindJob.java
@@ -0,0 +1,13 @@
+package com.hola.holalandwork.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SttWorkRequestFindJob {
+    private int sttWorkRequestFindJobId;
+    private String sttWorkRequestFindJobName;
+}
