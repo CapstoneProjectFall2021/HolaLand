48006cdf046a3850aaac9ecb4e9c0a1b928fb4ed
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:47:53 +0700

    code WorkRequestBookMapper

--- holaland-work/src/main/java/mapper/WorkRequestApplyMapper.java
+++ holaland-work/src/main/java/mapper/WorkRequestApplyMapper.java
@@ -1,4 +1,21 @@
 package mapper;
 
-public class WorkRequestApply {
+import com.hola.holalandwork.entity.WorkReport;
+import com.hola.holalandwork.entity.WorkRequestApply;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkRequestApplyMapper implements RowMapper<WorkRequestApply> {
+    @Override
+    public WorkRequestApply mapRow(ResultSet resultSet, int i) throws SQLException {
+        return WorkRequestApply.builder()
+                .workRequestApplyId(resultSet.getInt("work_request_apply_id"))
+                .accountId(resultSet.getInt("account_id"))
+                .workRequestRecruitmentId(resultSet.getInt("work_request_recruitment_id"))
+                .workRequestApplySttId(resultSet.getInt("work_request_apply_stt_id"))
+                .workRequestApplyDeleted(resultSet.getBoolean("work_request_apply_deleted"))
+                .build();
+    }
 }
