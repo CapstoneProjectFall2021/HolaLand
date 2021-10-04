fe9079d56e3dc275581bf03c7a3c14202e74e9be
Capstone Project <anhdaica0609@gmail.com>
2021-10-04 09:47:23 +0700

    add module work

---
+++ holaland-work/src/main/java/com/hola/holalandwork/HolalandWorkApplication.java
@@ -0,0 +1,12 @@
+package com.hola.holalandwork;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class HolalandWorkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolalandWorkApplication.class, args);
+    }
+
+}
---
+++ holaland-work/src/test/java/com/hola/holalandwork/HolalandWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hola.holalandwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolalandWorkApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
