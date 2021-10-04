2d301bf0b7ccafa0b15ad571503a35df4eb815e9
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:46:36 +0700

    code sttworkrequestfindjob mapper

--- holaland-work/src/main/java/mapper/SttWorkRequestFindJobMapper.java
+++ holaland-work/src/main/java/mapper/SttWorkRequestFindJobMapper.java
@@ -1,2 +1,19 @@
-package mapper;public class SttWorkRequestFindJobMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.SttWorkReport;
+import com.hola.holalandwork.entity.SttWorkRequestFindJob;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SttWorkRequestFindJobMapper implements RowMapper<SttWorkRequestFindJob> {
+
+    @Override
+    public SttWorkRequestFindJob mapRow(ResultSet resultSet, int i) throws SQLException {
+        return SttWorkRequestFindJob.builder()
+                .sttWorkRequestFindJobId(resultSet.getInt("stt_work_request_find_job_id"))
+                .sttWorkRequestFindJobName(resultSet.getString("stt_work_request_find_job_status"))
+                .build();
+    }
 }
