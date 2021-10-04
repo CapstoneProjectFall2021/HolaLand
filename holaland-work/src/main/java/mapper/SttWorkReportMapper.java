bd8d780e53f54548619ddfa849ce554b267fa585
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:46:52 +0700

    code SttWorkReportMapper

--- holaland-work/src/main/java/mapper/SttWorkReportMapper.java
+++ holaland-work/src/main/java/mapper/SttWorkReportMapper.java
@@ -1,2 +1,18 @@
-package mapper;public class SttWorkReportMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.SttWorkReport;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SttWorkReportMapper implements RowMapper<SttWorkReport> {
+    @Override
+    public SttWorkReport mapRow(ResultSet resultSet, int i) throws SQLException {
+
+        return SttWorkReport.builder()
+                .sttWorkReportId(resultSet.getInt("stt_work_report_id"))
+                .sttWorkReportContent(resultSet.getString("stt_work_report_content"))
+                .build();
+    }
 }
