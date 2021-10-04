a0cf18263cfe426b12621f66a11ed97861787082
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 22:10:46 +0700

    add Work Service Impl part 3

--- holaland-work/src/main/java/com/hola/holalandwork/service/impl/WorkRequestApplyServiceImpl.java
+++ holaland-work/src/main/java/com/hola/holalandwork/service/impl/WorkRequestApplyServiceImpl.java
@@ -1,4 +1,27 @@
 package com.hola.holalandwork.service.impl;
 
-public class WorkRequestApplyServiceImpl {
+import com.hola.holalandwork.entity.WorkRequestApply;
+import com.hola.holalandwork.repository.WorkRequestApplyRepository;
+import com.hola.holalandwork.service.WorkRequestApplyService;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+public class WorkRequestApplyServiceImpl implements WorkRequestApplyService {
+
+    private final WorkRequestApplyRepository workRequestApplyRepository;
+
+    public WorkRequestApplyServiceImpl(WorkRequestApplyRepository workRequestApplyRepository) {
+        this.workRequestApplyRepository = workRequestApplyRepository;
+    }
+
+    @Override
+    public List<WorkRequestApply> getAll() throws DataAccessException {
+        return workRequestApplyRepository.getAll();
+    }
+
+    @Override
+    public WorkRequestApply getOne(int id) throws DataAccessException {
+        return workRequestApplyRepository.getOne(id);
+    }
 }
