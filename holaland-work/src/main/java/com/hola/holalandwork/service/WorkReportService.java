0438d48ad4936030c1cac83f56373b896c0db179
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 20:41:29 +0700

    add  work service 6

---
+++ holaland-work/src/main/java/com/hola/holalandwork/service/WorkReportService.java
@@ -0,0 +1,10 @@
+package com.hola.holalandwork.service;
+
+import com.hola.holalandwork.entity.WorkReport;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+public interface WorkReportService {
+    List<WorkReport> getAll() throws DataAccessException;
+}
