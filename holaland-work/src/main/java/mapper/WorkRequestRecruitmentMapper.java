33a04a9c75409689c0505bf5501d8181aa714b98
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:48:30 +0700

    code WorkSalaryTypeMapper

--- holaland-work/src/main/java/mapper/WorkRequestRecruitmentMapper.java
+++ holaland-work/src/main/java/mapper/WorkRequestRecruitmentMapper.java
@@ -1,2 +1,34 @@
-package mapper;public class WorkRequestRecruitmentMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.WorkRequestFindJob;
+import com.hola.holalandwork.entity.WorkRequestRecruitment;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkRequestRecruitmentMapper implements RowMapper<WorkRequestRecruitment> {
+    @Override
+    public WorkRequestRecruitment mapRow(ResultSet resultSet, int i) throws SQLException {
+        return WorkRequestRecruitment.builder()
+                .workRequestRecruitmentId(resultSet.getInt("work_request_recruitment_id"))
+                .accountId(resultSet.getInt("account_id"))
+                .workSalaryTypeId(resultSet.getInt("work_salary_type_id"))
+                .workJobTypeId(resultSet.getInt("work_job_type_id"))
+                .sttWorkRequestRecruitmentId(resultSet.getInt("stt_work_request_recruitment_id"))
+                .workRequestRecruitmentTitle(resultSet.getString("work_request_recruitment_title"))
+                .workRequestRecruitmentStartDateTime(resultSet.getString("work_request_recruitment_start_date_time"))
+                .workRequestRecruitmentEndDateTime(resultSet.getString("work_request_recruitment_end_date_time"))
+                .workRequestRecruitmentLastUpdateDateTime(resultSet.getString("work_request_recruitment_last_update_date_time"))
+                .workRequestRecruitmentDescription(resultSet.getString("work_request_recruitment_description"))
+                .workRequestRecruitmentRequirement(resultSet.getString("work_request_recruitment_requirements"))
+                .workRequestRecruitmentBenefit(resultSet.getString("work_request_recruitment_benefits"))
+                .workRequestRecruitmentWage(resultSet.getString("work_request_recruitment_wage"))
+                .workRequestRecruitmentQuantity(resultSet.getInt("work_request_recruitment_quantity"))
+                .workRequestRecruitmentExperienceRequirement(resultSet.getString("work_request_recruitment_experience_required"))
+                .workRequestRecruitmentGenderRequirement(resultSet.getString("work_request_recruitment_gender_required"))
+                .workRequestRecruitmentWorkLocation(resultSet.getString("work_request_recruitment_work_location"))
+                .workRequestRecruitmentDeleted(resultSet.getBoolean("work_request_recruitment_deleted"))
+                .build();
+    }
 }
