46417fff8646717a1f13c58f4102ab2fd5c1e6d8
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:48:41 +0700

    code WorkTimeMapper

--- holaland-work/src/main/java/mapper/WorkSalaryTypeMapper.java
+++ holaland-work/src/main/java/mapper/WorkSalaryTypeMapper.java
@@ -1,2 +1,19 @@
-package mapper;public class WorkSalaryTypeMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.WorkRequestRecruitment;
+import com.hola.holalandwork.entity.WorkSalaryType;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkSalaryTypeMapper implements RowMapper<WorkSalaryType> {
+    @Override
+    public WorkSalaryType mapRow(ResultSet resultSet, int i) throws SQLException {
+        return WorkSalaryType.builder()
+                .workSalaryTypeId(resultSet.getInt("work_salary_type_id"))
+                .workSalaryTypeName(resultSet.getString("work_salary_type_name"))
+                .workSalaryTypeDeleted(resultSet.getBoolean("work_salary_type_deleted"))
+                .build();
+    }
 }
