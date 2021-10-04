c1b1ac1b5d688abdf8079d34da367d9af029d3e8
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 15:50:21 +0700

    add module hola-work

--- holaland-work/src/main/java/com/hola/holalandwork/HolalandWorkApplication.java
+++ holaland-work/src/main/java/com/hola/holalandwork/HolalandWorkApplication.java
@@ -1,8 +1,9 @@
 package com.hola.holalandwork;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootApplication
+@SpringBootApplication
 public class HolalandWorkApplication {
 
     public static void main(String[] args) {
