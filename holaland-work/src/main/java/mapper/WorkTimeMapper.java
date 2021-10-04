c9291f24b36c5763a239bff69901779de2d3860f
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:49:04 +0700

    fix file pom

--- holaland-work/src/main/java/mapper/WorkTimeMapper.java
+++ holaland-work/src/main/java/mapper/WorkTimeMapper.java
@@ -1,2 +1,18 @@
-package mapper;public class WorkTimeMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.WorkSalaryType;
+import com.hola.holalandwork.entity.WorkTime;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class WorkTimeMapper implements RowMapper<WorkTime> {
+    @Override
+    public WorkTime mapRow(ResultSet resultSet, int i) throws SQLException {
+        return WorkTime.builder()
+                .workTimeId(resultSet.getInt("work_time_id"))
+                .workTimeName(resultSet.getString("work_time_name"))
+                .build();
+    }
 }
