6b07d71db21231fb7bfb13fe51eb5e2b163d3e20
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 16:57:28 +0700

    add sttWorkReport entity

---
+++ holaland-work/src/main/java/com/hola/holalandwork/entity/SttWorkReport.java
@@ -0,0 +1,13 @@
+package com.hola.holalandwork.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SttWorkReport {
+    private int sttWorkReportId;
+    private String sttWorkReportContent;
+}
