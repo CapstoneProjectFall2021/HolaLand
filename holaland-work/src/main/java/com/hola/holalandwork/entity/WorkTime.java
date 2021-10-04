f7e6fbecb96314215c724dd0542398cbd8485c0c
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 15:58:20 +0700

    add workTime entity

---
+++ holaland-work/src/main/java/com/hola/holalandwork/entity/WorkTime.java
@@ -0,0 +1,13 @@
+package com.hola.holalandwork.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class WorkTime {
+    private int workTimeId;
+    private String workTimeName;
+}
