f25855af5e8e5a1c85083181e95bdb9c08b4bfa8
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 21:59:37 +0700

    Add Work Service  impl

--- holaland-work/src/main/java/com/hola/holalandwork/service/impl/WorkCommentServiceImpl.java
+++ holaland-work/src/main/java/com/hola/holalandwork/service/impl/WorkCommentServiceImpl.java
@@ -1,4 +1,24 @@
 package com.hola.holalandwork.service.impl;
 
-public class WorkCommentServiceImpl {
+import com.hola.holalandwork.entity.WorkComment;
+import com.hola.holalandwork.repository.WorkCommentRepository;
+import com.hola.holalandwork.service.WorkCommentService;
+import org.springframework.dao.DataAccessException;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class WorkCommentServiceImpl implements WorkCommentService {
+
+    private final WorkCommentRepository workCommentRepository;
+
+    public WorkCommentServiceImpl(WorkCommentRepository workCommentRepository) {
+        this.workCommentRepository = workCommentRepository;
+    }
+
+    @Override
+    public List<WorkComment> getAll() throws DataAccessException {
+        return workCommentRepository.getAll();
+    }
 }
