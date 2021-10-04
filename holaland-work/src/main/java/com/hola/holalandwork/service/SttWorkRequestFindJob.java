66ebbade117be649fc54a64c8b7ac7803d37aa0d
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 20:30:54 +0700

    add work service part 2

--- holaland-work/src/main/java/com/hola/holalandwork/service/SttWorkRequestFindJob.java
+++ holaland-work/src/main/java/com/hola/holalandwork/service/SttWorkRequestFindJob.java
@@ -1,2 +1,9 @@
-package com.hola.holalandwork.service;public interface SttWorkRequestFindJob {
+package com.hola.holalandwork.service;
+
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+public interface SttWorkRequestFindJob {
+    List<SttWorkRequestFindJob> getAll() throws DataAccessException;
 }
--- holaland-work/src/main/java/com/hola/holalandwork/service/WorkTimeService.java
+++ holaland-work/src/main/java/com/hola/holalandwork/service/WorkTimeService.java
@@ -1,2 +1,10 @@
-package com.hola.holalandwork.service;public interface WorkTimeService {
+package com.hola.holalandwork.service;
+
+import com.hola.holalandwork.entity.WorkTime;
+import org.springframework.dao.DataAccessException;
+
+import java.util.List;
+
+public interface WorkTimeService {
+    List<WorkTime> getAll() throws DataAccessException;
 }
