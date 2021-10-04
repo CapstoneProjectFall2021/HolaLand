8601acfb0227a419597e542c7b99bc5daeeb2498
Phạm Hữu Thọ <Thophhe130924@fpt.edu.vn>
2021-10-04 20:47:04 +0700

    code WorkCommentMapper

--- holaland-work/src/main/java/mapper/SttWorkRequestRecruitmentMapper.java
+++ holaland-work/src/main/java/mapper/SttWorkRequestRecruitmentMapper.java
@@ -1,2 +1,18 @@
-package mapper;public class SttWorkRequestRecruitmentMapper {
+package mapper;
+
+import com.hola.holalandwork.entity.SttWorkRequestRecruitment;
+import org.springframework.jdbc.core.RowMapper;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class SttWorkRequestRecruitmentMapper implements RowMapper<SttWorkRequestRecruitment> {
+
+    @Override
+    public SttWorkRequestRecruitment mapRow(ResultSet resultSet, int i) throws SQLException {
+        return SttWorkRequestRecruitment.builder()
+                .sttWorkRequestRecruitmentId(resultSet.getInt("stt_work_request_recruitment_id"))
+                .sttWorkRequestRecruitmentName(resultSet.getString("stt_work_request_recruitment_name"))
+                .build();
+    }
 }
