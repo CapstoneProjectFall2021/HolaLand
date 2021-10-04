64109720f0202b0bde71e6fa251cb338a145f39d
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-04 16:18:24 +0700

    Add  SttWorkRequestRecruitment

---
+++ holaland-work/src/main/java/com/hola/holalandwork/entity/SttWorkRequestRecruitment.java
@@ -0,0 +1,13 @@
+package com.hola.holalandwork.entity;
+
+import lombok.*;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@Getter
+@Setter
+public class SttWorkRequestRecruitment {
+    private int sttWorkRequestRecruitmentId;
+    private String sttWorkRequestRecruitmentName;
+}
