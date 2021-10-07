5b15fc7d9f178b874bf5953a87afa7de276219f8
Phạm Thế Anh <anhpthe130515@fpt.edu.vn>
2021-10-07 23:29:59 +0700

    edit member mapper

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/mapper/MemberMapper.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/mapper/MemberMapper.java
@@ -11,14 +11,7 @@ public class MemberMapper implements RowMapper<Member> {
     @Override
     public Member mapRow(ResultSet resultSet, int i) throws SQLException {
         return Member.builder()
-                .memberId(resultSet.getInt("member_id"))
-                .memberName(resultSet.getString("member_name"))
-                .memberGender(resultSet.getBoolean("member_gender"))
-                .memberDob(resultSet.getDate("member_dob"))
-                .memberMobile(resultSet.getString("member_mobile"))
-                .memberEmail(resultSet.getString("member_email"))
-                .memberRankId(resultSet.getInt("member_rank_id"))
-                .memberStatusId(resultSet.getInt("member_status_id"))
+                
                 .build();
     }
 }
