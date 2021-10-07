bd2866a624ed64ca6006fc3860011768b931a80e
Nguyễn Minh Tuấn <tuannmhe130922@fpt.edu.vn>
2021-10-07 20:44:36 +0700

    Update member service interface

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/service/impl/MemberServiceImpl.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/service/impl/MemberServiceImpl.java
@@ -14,11 +14,6 @@ public class MemberServiceImpl implements MemberService {
 
     private final MemberRepository memberRepository;
 
-    @Autowired
-    public MemberServiceImpl(MemberRepository memberRepository) {
-        this.memberRepository = memberRepository;
-    }
-
     @Override
     public List<Member> getAll() throws DataAccessException {
         return memberRepository.getAll();
