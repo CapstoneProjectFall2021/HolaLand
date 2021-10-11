f6669447bc3f3b10323ae61bde9064c7cb60d191
Nguyễn Minh Tuấn <tuannmhe130922@fpt.edu.vn>
2021-10-11 19:36:53 +0700

    drop member entity

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/entity/Member.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/entity/Member.java
@@ -4,19 +4,6 @@ import lombok.*;
 
 import java.sql.Date;
 
-@NoArgsConstructor
-@AllArgsConstructor
-@Builder
-@Getter
-@Setter
 public class Member {
-    private int memberId;
-    private String memberName;
-    private boolean memberGender;
-    private Date memberDob;
-    private String memberMobile;
-    private String memberEmail;
-    private int memberRankId;
-    private int memberStatusId;
 
 }
