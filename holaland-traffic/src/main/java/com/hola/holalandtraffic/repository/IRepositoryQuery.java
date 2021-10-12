07dbe2a6f0ea7546a7350bf909716de850ca68a8
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-10-12 22:33:56 +0700

    insert query member

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/IRepositoryQuery.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/IRepositoryQuery.java
@@ -17,4 +17,15 @@ public interface IRepositoryQuery {
     String MOTORBIKE_TAXI_DRIVERS_GET_ALL = "SELECT * FROM tf_motorbike_taxi_drivers WHERE tf_motorbike_taxi_drivers_deleted = 0";
     String MOTORBIKE_TAXI_DRIVERS_GET_ONE = "SELECT * FROM tf_motorbike_taxi_drivers WHERE tf_motorbike_taxi_drivers_id = ? AND tf_motorbike_taxi_drivers_deleted = 0";
 
+
+
+
+    String MEMBER_GET_ALL = "SELECT * FROM member";
+    String MEMBER_GET_ONE = "SELECT * FROM member WHERE member_id = ?";
+    String MEMBER_ADD_ONE = "INSERT INTO member (member_name, member_gender, member_dob, member_mobile, member_email, member_rank_id, member_status_id)\n" +
+            "VALUES (?, ?, ?, ?, ?, ?, ?)";
+    String MEMBER_UPDATE_ONE = "UPDATE member\n" +
+            "SET member_name = ?, member_gender = ?, member_dob = ?, member_mobile = ?, member_email = ?, member_rank_id = ?, member_status_id = ?\n" +
+            "WHERE member_id = ?";
+    String MEMBER_DELETE_ONE = "DELETE FROM member WHERE member_id = ?";
 }
