2d51d6b7daac7827bea59d31263f501cbfaee1e0
Capstone Project <anhdaica0609@gmail.com>
2021-10-23 15:50:30 +0700

    add module entertaiment

---
+++ holaland-entertainment/src/main/java/com/hola/holalandentertainment/HolalandEntertainmentApplication.java
@@ -0,0 +1,12 @@
+package com.hola.holalandentertainment;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class HolalandEntertainmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolalandEntertainmentApplication.class, args);
+    }
+
+}
---
+++ holaland-entertainment/src/test/java/com/hola/holalandentertainment/HolalandEntertainmentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hola.holalandentertainment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolalandEntertainmentApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
