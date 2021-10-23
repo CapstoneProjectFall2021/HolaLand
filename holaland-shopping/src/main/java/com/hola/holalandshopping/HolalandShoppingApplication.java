2c482c09c85f6a78492d7a76579a237d18a0d54b
Capstone Project <anhdaica0609@gmail.com>
2021-10-23 16:06:02 +0700

    add module shopping

---
+++ holaland-shopping/src/main/java/com/hola/holalandshopping/HolalandShoppingApplication.java
@@ -0,0 +1,12 @@
+package com.hola.holalandshopping;
+
+import org.springframework.boot.SpringApplication;
+
+//@SpringBootApplication
+public class HolalandShoppingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HolalandShoppingApplication.class, args);
+    }
+
+}
---
+++ holaland-shopping/src/test/java/com/hola/holalandshopping/HolalandShoppingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hola.holalandshopping;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HolalandShoppingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
