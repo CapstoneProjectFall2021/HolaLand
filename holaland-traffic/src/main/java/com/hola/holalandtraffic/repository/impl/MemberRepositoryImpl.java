421759156e3a19b13c81ae64d1e15e9819480577
Nguyễn Minh Tuấn <tuannmhe130922@fpt.edu.vn>
2021-10-29 22:13:14 +0700

    fix repository impl

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/impl/MemberRepositoryImpl.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/impl/MemberRepositoryImpl.java
@@ -36,6 +36,23 @@ public class MemberRepositoryImpl implements MemberRepository, IRepositoryQuery 
     }
 
     @Override
+    public int save(Member obj) throws DataAccessException {
+        KeyHolder keyHolder = new GeneratedKeyHolder();
+        jdbcTemplate.update(connection -> {
+            PreparedStatement ps = connection.prepareStatement(MEMBER_ADD_ONE, Statement.RETURN_GENERATED_KEYS);
+            ps.setObject(1, obj.getMemberName());
+            ps.setObject(2, obj.isMemberGender());
+            ps.setObject(3, obj.getMemberDob());
+            ps.setObject(4, obj.getMemberMobile());
+            ps.setObject(5, obj.getMemberEmail());
+            ps.setObject(6, obj.getMemberRankId());
+            ps.setObject(7, obj.getMemberStatusId());
+            return ps;
+        }, keyHolder);
+        return keyHolder.getKey().intValue();
+    }
+
+    @Override
     public boolean update(Member obj) throws DataAccessException {
         return jdbcTemplate.update(
                 MEMBER_UPDATE_ONE,
