01c32635c4110db36730af1fecdbdaa4919a8828
nguyen hoang duc <ducnhse06110@fpt.edu.vn>
2021-11-01 21:42:31 +0700

    cod motorbike

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/entity/MotorbikeTaxiDrivers.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/entity/MotorbikeTaxiDrivers.java
@@ -9,6 +9,16 @@ import lombok.*;
 @Setter
 public class MotorbikeTaxiDrivers {
 
-    
+    private int tfMotorbikeTaxiDriversId;
+    private String tfMotorbikeTaxiDriversName;
+    private boolean tfMotorbikeTaxiDriversGender;
+    private String tfMotorbikeTaxiDriversPhone;
+    private String tfMotorbikeTaxiDriversImage;
+    private String tfMotorbikeTaxiDriversLicensePlates;
+    private String tfMotorbikeTaxiDriversVehicleType;
+    private String tfMotorbikeTaxiDriversStartTime;
+    private String tfMotorbikeTaxiDriversEndTime;
+    private double tfMotorbikeTaxiDriversRating;
+    private int tfMotorbikeTaxiDriversStatus;
     private boolean tfMotorbikeTaxiDriversDeleted;
 }
