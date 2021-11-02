99f245d3c5577d7549b411df88c0e1d849e3ecd5
nguyen hoang duc <ducnhse06110@fpt.edu.vn>
2021-11-02 13:27:09 +0700

    mapping send email

--- holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
@@ -47,7 +47,16 @@ public class MemberController {
     }
 
     @GetMapping("/send-email")
-    
+    public String sendEmail(@RequestParam("id") Integer id, Model model) {
+        Member member = memberService.getOne(id);
+        sendEmailService.send(
+                "HolaLand",
+                "Mã kích hoạt của bạn là: 123456xxx",
+                member.getMemberEmail()
+        );
+        model.addAttribute("send", "- Gửi email thành công!");
+        backToMembers(model);
+        return "members";
     }
 
     //add member
