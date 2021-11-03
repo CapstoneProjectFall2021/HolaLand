840c8bf584f35a657bd3cf009746139173b1fde1
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-11-03 18:53:28 +0700

    add query module traffic

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/IRepositoryQuery.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/IRepositoryQuery.java
@@ -2,7 +2,10 @@ package com.hola.holalandtraffic.repository;
 
 public interface IRepositoryQuery {
 //
-    
+    String MEMBER_GET_ALL = "SELECT * FROM member";
+    String MEMBER_GET_ONE = "SELECT * FROM member WHERE member_id = ?";
+    String MEMBER_ADD_ONE = "INSERT INTO member (member_name, member_gender, member_dob, member_mobile, member_email, member_rank_id, member_status_id)\n" +
+                            "VALUES (?, ?, ?, ?, ?, ?, ?)";
     String MEMBER_UPDATE_ONE = "UPDATE member\n" +
                                "SET member_name = ?, member_gender = ?, member_dob = ?, member_mobile = ?, member_email = ?, member_rank_id = ?, member_status_id = ?\n" +
                                "WHERE member_id = ?";
