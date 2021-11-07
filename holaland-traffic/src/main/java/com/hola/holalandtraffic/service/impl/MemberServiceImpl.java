23db69d9cb7b44e4d0a8ecf8d1ab463e540dc766
Nguyễn Minh Tuấn <tuannmhe130922@fpt.edu.vn>
2021-11-07 21:06:56 +0700

    fix service

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/service/impl/MemberServiceImpl.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/service/impl/MemberServiceImpl.java
@@ -19,5 +19,28 @@ public class MemberServiceImpl implements MemberService {
         this.memberRepository = memberRepository;
     }
 
-    
+    @Override
+    public List<Member> getAll() throws DataAccessException {
+        return memberRepository.getAll();
+    }
+
+    @Override
+    public Member getOne(int id) throws DataAccessException {
+        return memberRepository.getOne(id);
+    }
+
+    @Override
+    public int save(Member obj) throws DataAccessException {
+        return memberRepository.save(obj);
+    }
+
+    @Override
+    public boolean update(Member obj) throws DataAccessException {
+        return memberRepository.update(obj);
+    }
+
+    @Override
+    public boolean delete(int id) throws DataAccessException {
+        return memberRepository.delete(id);
+    }
 }
