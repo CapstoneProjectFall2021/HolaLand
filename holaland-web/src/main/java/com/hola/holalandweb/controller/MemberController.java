84b3bf26dee06dca1a9c383478ec29211286dbc5
nguyen hoang duc <ducnhse06110@fpt.edu.vn>
2021-11-08 20:57:46 +0700

    fix find job

--- holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
@@ -47,7 +47,13 @@ public class MemberController {
     }
 
     @GetMapping("/send-email")
-    
+    public String sendEmail(@RequestParam("id") Integer id, Model model) {
+        Member member = memberService.getOne(id);
+        sendEmailService.send(
+                "HolaLand",
+                "Mã kích hoạt của bạn là: 123456xxx",
+                
+        );
         model.addAttribute("send", "- Gửi email thành công!");
         backToMembers(model);
         return "members";
