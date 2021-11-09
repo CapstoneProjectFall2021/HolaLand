dbab15a486f7b6d2065eca6bfec9e71df390d25f
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-11-09 21:47:00 +0700

    Fix query module fpt

--- holaland-fptu/src/main/java/com/hola/holalandfptu/repository/IRepositoryQuery.java
+++ holaland-fptu/src/main/java/com/hola/holalandfptu/repository/IRepositoryQuery.java
@@ -2,9 +2,5 @@ package com.hola.holalandfptu.repository;
 
 public interface IRepositoryQuery {
 
-    String CLUB_TYPE_GET_ALL = "SELECT * FROM fptu_club_type WHERE fptu_club_type_deleted = 0";
-    String CLUB_TYPE_GET_ONE = "SELECT * FROM fptu_club_type WHERE fptu_club_type_id = ? AND fptu_club_type_deleted = 0";
-
-    String CLUB_GET_ALL_BY_TYPE = "SELECT * FROM fptu_club WHERE fptu_club_type_id = ? AND fptu_club_deleted = 0";
-    String CLUB_GET_ONE = "SELECT * FROM fptu_club WHERE fptu_club_id = ? AND fptu_club_deleted = 0";
+    
 }
