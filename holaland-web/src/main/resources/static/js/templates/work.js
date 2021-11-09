49d3b37dc69a56d7e7224e48157d6dc59959ec38
Capstone Project <suncho2511@gmail.com>
2021-11-09 07:18:24 +0700

    commit

--- holaland-web/src/main/resources/static/js/templates/work.js
+++ holaland-web/src/main/resources/static/js/templates/work.js
@@ -1,15 +1,20 @@
 /*
  *
  */
-
 function openModal(modal) {
   const myModal = new bootstrap.Modal(document.getElementById(modal));
   myModal.show();
 }
 
-//Recruitment manage
+function showToast(id) {
+  const toast = new bootstrap.Toast(document.getElementById(id));
+  toast.show();
+}
 
 
+/*
+ * Recruitment manage
+ */
 function confirmDeleteRequestApply(e) {
   const requestId = e.target.firstElementChild.innerHTML;
   openModal("confirmCancelApplyJobModal");
@@ -26,28 +31,56 @@ function confirmRemoveRecruitmentRequest(e) {
   const requestId = e.target.firstElementChild.innerHTML;
   let url = new URL(window.location.href);
   let sttWorkCode = 1;
-  if(url.searchParams.has("code")) {
+  if (url.searchParams.has("code")) {
     sttWorkCode = url.searchParams.get("code");
   }
-  openModal("confirmRemoveRecruitmentRequestModal");
 
+  openModal("confirmRemoveRecruitmentRequestModal");
   document.getElementById("btn-delete-recruitment-request").href =
-      "/works/request-recruitment-manage/delete?requestId=" + requestId + "&code=" + sttWorkCode;
+    "/works/request-recruitment-manage/delete?requestId=" + requestId + "&code=" + sttWorkCode;
 }
 
-function confirmRemoveFindRequest(e) {
+function confirmRemoveFindJobRequest(e) {
   const requestId = e.target.firstElementChild.innerHTML;
   let url = new URL(window.location.href);
   let sttWorkCode = 1;
-  if(url.searchParams.has("code")) {
+  if (url.searchParams.has("code")) {
     sttWorkCode = url.searchParams.get("code");
   }
-  openModal("confirmRemoveFindRequestModal");
 
+  openModal("confirmRemoveFindJobRequestModal");
   document.getElementById("btn-delete-find-request").href =
-      "/works/request-find-job-manage/delete?requestId=" + requestId + "&code=" + sttWorkCode;
+    "/works/request-find-job-manage/delete?requestId=" + requestId + "&code=" + sttWorkCode;
 }
 
 function reasonRejectRequest() {
   openModal("reasonRejectModal");
+}
+
+
+/*
+ * Recruitment manage
+ */
+function saveRequestRecruitment() {
+  const request = new XMLHttpRequest();
+  request.open("GET", "https://anest-hl-backend.herokuapp.com/course/1", false);
+  request.onreadystatechange = function () {
+    if (this.readyState === 4 && this.status === 200) {
+      // document.getElementById("result").innerHTML = this.responseText;
+      setStateBtnSaveJob();
+      showToast('toast-success');
+    } else {
+      alert("ERROR");
+    }
+  };
+  request.send();
+}
+
+function setStateBtnSaveJob() {
+  document.getElementById("label-save-request").innerHTML = "ĐÃ LƯU";
+  document.getElementById("icon-save-request").classList.remove('far');
+  document.getElementById("icon-save-request").classList.add('fas');
+  document.getElementById("btn-save-request").onclick = function (e) {
+    e.defaultPrevented();
+  }
 }
