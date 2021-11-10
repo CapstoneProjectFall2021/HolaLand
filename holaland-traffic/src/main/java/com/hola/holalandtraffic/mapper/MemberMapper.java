0764ea6b62a8c46cf2a04fa364af3d9015e5d33d
Nguyễn Minh Tuấn <tuannmhe130922@fpt.edu.vn>
2021-11-10 21:41:32 +0700

    update mapper

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/mapper/MemberMapper.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/mapper/MemberMapper.java
@@ -9,15 +9,6 @@ import java.sql.SQLException;
 public class MemberMapper implements RowMapper<Member> {
     @Override
     public Member mapRow(ResultSet resultSet, int i) throws SQLException {
-        return Member.builder()
-                .memberId(resultSet.getInt("member_id"))
-                .memberName(resultSet.getString("member_name"))
-                .memberGender(resultSet.getBoolean("member_gender"))
-                .memberDob(resultSet.getDate("member_dob"))
-                .memberMobile(resultSet.getString("member_mobile"))
-                .memberEmail(resultSet.getString("member_email"))
-                .memberRankId(resultSet.getInt("member_rank_id"))
-                .memberStatusId(resultSet.getInt("member_status_id"))
-                .build();
+        
     }
 }
