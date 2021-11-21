92c1604263e298bf035c11cae2bf0e3c6e179e49
Capstone Project <suncho2511@gmail.com>
2021-11-21 22:47:57 +0700

    add profile

--- holaland-web/src/main/java/com/hola/holalandweb/controller/ProfileController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/ProfileController.java
@@ -1,13 +1,21 @@
 package com.hola.holalandweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
 
     @GetMapping("/profile")
-    public String profile() {
+    public String profile(Model model) {
+        model.addAttribute("page", 1);
+        return "profile";
+    }
+
+    @GetMapping("/profile-update")
+    public String profileUpdate(Model model) {
+        model.addAttribute("page", 3);
         return "profile";
     }
 }
