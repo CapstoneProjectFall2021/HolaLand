1d7f7019bc666d9b11ec15f6f27eb6508d9a0f3f
Capstone Project <suncho2511@gmail.com>
2021-11-21 23:05:49 +0700

    example

--- holaland-web/src/main/java/com/hola/holalandweb/controller/ProfileController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/ProfileController.java
@@ -1,5 +1,7 @@
 package com.hola.holalandweb.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -7,9 +9,21 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ProfileController {
 
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    public ProfileController(PasswordEncoder passwordEncoder) {
+        this.passwordEncoder = passwordEncoder;
+    }
+
     @GetMapping("/profile")
     public String profile(Model model) {
         model.addAttribute("page", 1);
+
+        System.out.println(passwordEncoder.encode("123456"));
+
+        // check old password
+        System.out.println(passwordEncoder.matches("123456", "$2a$10$S5XdhGZdPq5eqkI6DLez6ucT4JywAFhltqz8h7sMRT5xvh1kVkHFK"));
         return "profile";
     }
 
