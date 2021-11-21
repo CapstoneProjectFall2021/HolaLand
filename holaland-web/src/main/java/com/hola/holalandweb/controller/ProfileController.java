2b2ff01659a64e615b9f1bc44be17267054d67eb
Capstone Project <suncho2511@gmail.com>
2021-11-21 22:29:10 +0700

    cm

--- holaland-web/src/main/java/com/hola/holalandweb/controller/ProfileController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/ProfileController.java
@@ -1,7 +1,13 @@
 package com.hola.holalandweb.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ProfileController {
+
+    @GetMapping("/profile")
+    public String profile() {
+        return "profile";
+    }
 }
