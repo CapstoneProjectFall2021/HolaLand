8c0c3baeb1b5d8dd5ed172679647d51e62e49383
Đoàn Quốc Việt <vietdqhe130470@fpt.edu.vn>
2021-11-24 22:26:37 +0700

    fix query

--- holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/IRepositoryQuery.java
+++ holaland-traffic/src/main/java/com/hola/holalandtraffic/repository/IRepositoryQuery.java
@@ -6,10 +6,7 @@ public interface IRepositoryQuery {
     String MEMBER_GET_ONE = "SELECT * FROM member WHERE member_id = ?";
     String MEMBER_ADD_ONE = "INSERT INTO member (member_name, member_gender, member_dob, member_mobile, member_email, member_rank_id, member_status_id)\n" +
                             "VALUES (?, ?, ?, ?, ?, ?, ?)";
-    String MEMBER_UPDATE_ONE = "UPDATE member\n" +
-                               "SET member_name = ?, member_gender = ?, member_dob = ?, member_mobile = ?, member_email = ?, member_rank_id = ?, member_status_id = ?\n" +
-                               "WHERE member_id = ?";
-    String MEMBER_DELETE_ONE = "DELETE FROM member WHERE member_id = ?";
+    
 
     String BUS_GET_ALL = "SELECT * FROM tf_bus WHERE tf_bus_deleted = 0";
     String BUS_GET_ONE = "SELECT * FROM tf_bus WHERE tf_bus_id = ? AND tf_bus_deleted = 0";
