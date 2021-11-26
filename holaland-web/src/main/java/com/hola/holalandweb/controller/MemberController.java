49893aec08174b702a41174797d537a0b751e7f9
nguyen hoang duc <ducnhse06110@fpt.edu.vn>
2021-11-26 16:18:41 +0700

    update mapping

--- holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
@@ -43,7 +43,7 @@ public class MemberController {
     @GetMapping("/members")
     public String members(Model model) {
         backToMembers(model);
-        
+        return "members";
     }
 
     @GetMapping("/send-email")
