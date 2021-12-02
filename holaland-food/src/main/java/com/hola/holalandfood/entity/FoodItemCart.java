263be7e995f443443543fcb6aa02814b76240fc8
Capstone Project <suncho2511@gmail.com>
2021-12-02 19:48:25 +0700

    add to carrt

--- holaland-food/src/main/java/com/hola/holalandfood/entity/FoodItemCart.java
+++ holaland-food/src/main/java/com/hola/holalandfood/entity/FoodItemCart.java
@@ -5,16 +5,21 @@ import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
+
+import java.io.Serializable;
 
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
 @Getter
 @Setter
-public class FoodItemCart {
+@ToString
+public class FoodItemCart implements Serializable {
 
     private int foodId;
     private String foodName;
     private double unitPrice;
     private int quantity;
+    private double totalPrice;
 }
--- holaland-web/src/main/java/com/hola/holalandweb/module/food/controller/FoodOrderController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/module/food/controller/FoodOrderController.java
@@ -7,10 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpSession;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 @RequestMapping("/food/order")
@@ -24,19 +27,45 @@ public class FoodOrderController {
     }
 
     @GetMapping("add-to-cart")
-    public ResponseEntity<?> getCourseById(@RequestParam("foodId") int foodId, @RequestParam("storeId") int storeId) {
-        System.out.println("\n\n\n" + foodId + " - " + storeId + "\n\n\n");
-
+    public ResponseEntity<?> getCourseById(
+            @RequestParam("foodId") int foodId,
+            @RequestParam("storeId") int storeId,
+            HttpSession session
+    ) {
         FoodItem foodItem = foodItemService.getOne(foodId);
         FoodItemCart foodItemCart = FoodItemCart.builder()
                 .foodId(foodId)
                 .foodName(foodItem.getFoodItemName())
+                .unitPrice(foodItem.getFoodItemPrice())
+                .quantity(1)
+                .totalPrice(foodItem.getFoodItemPrice())
                 .build();
 
-        if(true) {
-            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        List<FoodItemCart> listFoodOrder = null;
+        listFoodOrder = (ArrayList<FoodItemCart>) session.getAttribute("listFoodOrder");
+
+        boolean flat = true;
+        if (listFoodOrder == null) {
+            listFoodOrder = new ArrayList<>();
+            listFoodOrder.add(foodItemCart);
+            session.setAttribute("listFoodOrder", listFoodOrder);
         } else {
-            return new ResponseEntity<>(HttpStatus.OK);
+            for (FoodItemCart f : listFoodOrder) {
+                if (f.getFoodId() == foodId) {
+                    f.setQuantity(f.getQuantity() + 1);
+                    f.setTotalPrice(f.getQuantity() * f.getUnitPrice());
+                    flat = false;
+                }
+            }
+            if (flat) {
+                listFoodOrder.add(foodItemCart);
+                flat = true;
+            }
+            session.setAttribute("listFoodOrder", listFoodOrder);
         }
+
+        listFoodOrder.forEach(s -> System.out.println(s));
+
+        return new ResponseEntity<>(listFoodOrder.size(), HttpStatus.OK);
     }
 }
