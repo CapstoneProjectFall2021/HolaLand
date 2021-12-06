8e0074b9c0c3cc59c2b820a1fc44fd51d235d973
Capstone Project <suncho2511@gmail.com>
2021-12-06 23:22:53 +0700

    fix store

--- holaland-web/src/main/java/com/hola/holalandweb/module/food/controller/FoodManageStoreController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/module/food/controller/FoodManageStoreController.java
@@ -1,4 +1,4 @@
-package com.hola.holalandweb.controller;
+package com.hola.holalandweb.module.food.controller;
 
 import com.hola.holalandcore.entity.CustomUser;
 import com.hola.holalandfood.entity.FoodItem;
