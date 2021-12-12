2e421fe51d7f4f4a62fd1b5a55a79b288c726128
nguyen hoang duc <ducnhse06110@fpt.edu.vn>
2021-12-12 14:24:18 +0700

    fix quanlity of report

--- holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
+++ holaland-web/src/main/java/com/hola/holalandweb/controller/MemberController.java
@@ -29,11 +29,7 @@ public class MemberController {
     @Autowired
     public MemberController(MemberService memberService, SendEmailService sendEmailService, BusService busService, MotorbikeTaxiDriversService motorbikeTaxiDriversService,
                             ClubService clubService, ClubTypeService clubTypeService) {
-        this.memberService = memberService;
-        this.sendEmailService = sendEmailService;
-        this.busService = busService;
-        this.clubService = clubService;
-        this.clubTypeService = clubTypeService;
+        
     }
 
     @GetMapping("/send-email")
